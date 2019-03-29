a8a4a3ce17b40f930755605f79b35782c2f1534a
Amir Mirzaeian <amir.mirzaeian2012@gmail.com>
2019-03-29 17:59:09 +0100

    March 29, 2019

---
+++ src/com/amir/java/exercise/oddoreven/OddOrEven.java
@@ -0,0 +1,29 @@
+package com.amir.java.exercise.oddoreven;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input:");
+
+        try{
+
+            int num = scanner.nextInt();
+
+            if (num%2 == 0 ){
+                System.out.println("The input is even");
+            } else {
+                System.out.println("The input is odd");
+            }
+
+        } catch (InputMismatchException e){
+            System.out.println("You need to enter an integer. Bye");
+            System.exit(-1);
+        }
+
+    }
+    }
+
